7a33e42bbc76582a3ebece90981531d1f5380267
Hibikero <1261458973@qq.com>
2024-12-22 23:13:11 +0800

    11

--- src/main/java/hibikero/lobotomplugin/LobotomPlugin.java
+++ src/main/java/hibikero/lobotomplugin/LobotomPlugin.java
@@ -1,17 +1,26 @@
 package hibikero.lobotomplugin;
 
+import hibikero.lobotomplugin.BackEnd.Command.SpawnCustomCommand;
+import hibikero.lobotomplugin.BackEnd.Entities.Bodi.BodiEntity;
+import hibikero.lobotomplugin.BackEnd.Entities.CustomEntityManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LobotomPlugin extends JavaPlugin {
-
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        // 注册自定义生物
+        CustomEntityManager.registerEntity(new BodiEntity());
+        
+        // 注册命令
+        SpawnCustomCommand spawnCommand = new SpawnCustomCommand();
+        getCommand("spawnmob").setExecutor(spawnCommand);
+        getCommand("spawnmob").setTabCompleter(spawnCommand);
+        
+        getLogger().info("LobotomPlugin 已启用!");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("LobotomPlugin 已禁用!");
     }
 }
